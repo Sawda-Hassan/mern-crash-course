498fddd430b6e80680be5f998a899abc12127c09
Sawda-Hassan <milgomaariin@gmail.com>
2025-12-15 23:18:27 +0300

    added render

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
+export const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error("Error: MONGO_URI is undefined. Set it in Render environment variables!");
+    process.exit(1);
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
